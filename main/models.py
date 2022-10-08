d5681a67f976b87d0bf48129b70581bbe67e39d3
ben745260 <106643280+ben745260@users.noreply.github.com>
2022-10-08 20:50:47 +0800

    Update model datetime

--- main/models.py
+++ main/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from datetime import datetime
+from django.utils import timezone
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE, DO_NOTHING, SET_NULL
+from django.db.models.expressions import F
 
 # Create your models here.
 class HealthData(models.Model):
-    data_temperature =models.CharField(max_length=32)
-    data_heartBeat =models.CharField(max_length=32)
-    data_bloodPressure =models.CharField(max_length=32)
-    remark= models.TextField(max_length=8192)
-    saveDate = models.DateTimeField("date punished",default=datetime.now())
+    data_temperature = models.CharField(max_length=32)
+    data_heartBeat = models.CharField(max_length=32)
+    data_bloodPressure = models.CharField(max_length=32)
+    remark = models.TextField(max_length=8192)
+    saveDate = models.DateTimeField(default=datetime.now)
     
-    def _str_(self):
-        return f"{self.id}: {self.saveDate}"
+    def __str__(self):
+        return f"{self.id}: {self.saveDate} {self.remark}"
