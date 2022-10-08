f24f4b6744614dc8e8ea28ea69835c1d0a49ede2
ben745260 <106643280+ben745260@users.noreply.github.com>
2022-10-08 20:03:05 +0800

    Add models HealthData and set up admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import HealthData
+from django.db import models
 # Register your models here.
+
+admin.site.register(HealthData)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class HealthData(models.Model):
+    data_temperature =models.CharField(max_length=32)
+    data_heartBeat =models.CharField(max_length=32)
+    data_bloodPressure =models.CharField(max_length=32)
+    remark= models.TextField(max_length=8192)
+    saveDate = models.DateTimeField("date punished",default=datetime.now())
+    
+    def _str_(self):
+        return f"{self.id}: {self.saveDate}"
